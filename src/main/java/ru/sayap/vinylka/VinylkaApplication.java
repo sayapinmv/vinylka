8d3598c889bec6ac9c45de8fefd3a594fbe28bc6
sayapinmv <sayapin.05@list.ru>
2025-02-11 01:50:38 +0300

    sayapinmv

---
+++ src/main/java/ru/sayap/vinylka/VinylkaApplication.java
@@ -0,0 +1,13 @@
+package ru.sayap.vinylka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinylkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VinylkaApplication.class, args);
+    }
+
+}
